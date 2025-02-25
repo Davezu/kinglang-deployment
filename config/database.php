19e2624dbbdc1d587ec8313bf70fe9f44d937e6c
JericKen <vjericken@gmail.com>
2025-02-25 20:38:17 +0800

    Initial Commit

--- config/database.php
+++ config/database.php
@@ -4,9 +4,10 @@ $user = "root";
 $password = "";
 $database = "kinglang_booking";
 
-$conn = new mysqli($host, $user, $password, $database);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+try {
+    $pdo = new PDO("mysql:host$host;dbname=$database;charset=utf8", $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOExeption $e) {
+    die("Database connection failed: " . $e->getMessage());
 }
 ?>
